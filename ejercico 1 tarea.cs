2434d028568925a525c3767a825ee707585ac52f
djei211 <you@example.com>
2023-10-01 21:14:41 -0600

    ejercicios 1 2 3 de la tarea

---
+++ ejercico 1 tarea.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercio1progra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la tienda de camisas");
+
+            
+            Console.Write("Ingrese la cantidad de camisas que desea comprar: ");
+            int cantidadCamisas = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el precio unitario de las camisas: ");
+            double precioUnitario = double.Parse(Console.ReadLine());
+
+            
+            double precioTotal = cantidadCamisas * precioUnitario;
+
+            
+            double descuento = 0;
+
+            if (cantidadCamisas >= 2 && cantidadCamisas <= 5)
+            {
+                descuento = precioTotal * 0.15;
+            }
+            else if (cantidadCamisas > 5)
+            {
+                descuento = precioTotal * 0.20;
+            }
+
+            
+            double precioTotalConDescuento = precioTotal - descuento;
+
+            
+            Console.WriteLine($"El precio total con descuento es: ${precioTotalConDescuento:N2}");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ ejercio 2 y 3 tarea.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejericio3_tarea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese la cantidad de artículos a comprar: ");
+            int cantidadArticulos = Convert.ToInt32(Console.ReadLine());
+
+            double precioPorArticulo;
+
+            if (cantidadArticulos <= 10)
+            {
+                precioPorArticulo = 20.0;
+            }
+            else
+            {
+                precioPorArticulo = 15.0;
+            }
+
+            double total = cantidadArticulos * precioPorArticulo;
+
+            Console.WriteLine($"Precio por artículo: ${precioPorArticulo}");
+            Console.WriteLine($"Total a pagar: ${total}");
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
